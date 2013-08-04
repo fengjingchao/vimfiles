c651560fa12f79b41f54256c3134a3bb14730b5f
Hongchao Deng <fengjingchao@gmail.com>
2013-08-03 21:23:47 -0700

    youcompleteme

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 curdir=`pwd`
 
+rm ~/.vim
 ln -s $curdir ~/.vim
+rm ~/.vimrc
 ln -s $curdir/vimrc ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+sh ./bundle/YouCompleteMe/install.sh
