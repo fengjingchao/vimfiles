543bb866ad6e05034d6a536c3987d6dff865dc0a
Hongchao Deng <fengjingchao@gmail.com>
2013-08-03 21:26:30 -0700

    refine install.sh with clang support for ycm

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ rm ~/.vimrc
 ln -s $curdir/vimrc ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
-sh ./bundle/YouCompleteMe/install.sh
+sh ./bundle/YouCompleteMe/install.sh --clang-completer
