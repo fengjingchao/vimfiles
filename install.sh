07e1abd53aed289d879ab674d7aab80d43600fd6
Hongchao Deng <fengjingchao@gmail.com>
2014-05-10 15:06:31 -0400

    vim using ycm only

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Choose the option you want
-# 1. neocomplete
-# 2. youcompleteme
-#
-# Then
-# cp vim.bundle[#] vim.bundle
-# cp vimrc[#] vimrc
-
 mkdir -p ~/.vimtmp/swap
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
@@ -15,5 +7,3 @@ sh $GOPATH/src/github.com/nsf/gocode/vim/update.sh
 
 # go get -u code.google.com/p/go.tools/cmd/goimports
 # go get -u github.com/nsf/gocode
-#
-# rm after/ if using ycm
